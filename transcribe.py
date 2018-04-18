c3471770301212acf6db6e8e05e6364edd68b91a
cqueriap <38477099+cqueriap@users.noreply.github.com>
2018-04-18 01:12:58 -0500

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,3 +1,9 @@
+'''
+Created a file named transcribe.py. In this file, I created a function
+named dna to rna that takes one parameter. The function should translate the template
+DNA sequence to its mRNA and return the mRNA.
+'''
+
 def dna_to_rna(dna):
     rna = ''
     for letter in dna:
